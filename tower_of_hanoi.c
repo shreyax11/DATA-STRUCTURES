8376bb166b6f243262a83e961c6b552fb50271a2
shreyax11 <shreya.gpt.1101@gmail.com>
2026-01-15 18:19:44 +0530

    Update tower_of_hanoi.c

--- tower_of_hanoi.c
+++ tower_of_hanoi.c
@@ -1,23 +1,21 @@
 #include<stdio.h>
-
-void toh(int n, char from, char to, char aux);
-
-void main()
+void toh(int n,char from_rod,char to_rod,char aux_rod)
 {
-	int n;
-	printf("Enter number of disks:");
-	scanf("%d",&n);
-	toh(n,'A','B','C');
-}
-
-void toh(int n,char from, char to, char aux)
-{
-	if(n == 1)
+	if(n==1)
 	{
-		printf("\nMove disk 1 from rod %c to rod %c\n", from, to);
+		printf("\n move disk 1 from rod %c to rod %c",from_rod,to_rod);
 		return;
 	}
-	toh(n-1,from,aux,to);
-	printf("\nMove disk %d from rod %c to rod %c\n",n,from,to);
-	toh(n-1,aux,to,from);
+	toh(n-1,from_rod,aux_rod,to_rod);
+	printf("\n move disk %d from rod %c to rod %c",n,from_rod,to_rod);
+	toh(n-1,aux_rod,to_rod,from_rod);
+}
+int main()
+{
+    int n;
+    printf("enter the disks:");
+    scanf("%d",&n);
+	toh(n,'a','b','c');
+	printf("\n");
+    return 0;
 }
